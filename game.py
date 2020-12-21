096a712e5520d8dbaf33e3e4d5a08ce6cacd5bc9
irinache <irina.che333@gmail.com>
2020-12-21 23:42:22 +0200

    Added Santa`s moves handling

--- game.py
+++ game.py
@@ -1,15 +1,17 @@
-from new_year_game.button import *
 from new_year_game.colors import *
 from new_year_game.game_screen import GameScreen
 from new_year_game.menu_screen import MenuScreen
 from new_year_game.santa import Santa
 from new_year_game.text import Text
 
+import pygame
+from pygame import event
 
 class Game:
     def __init__(self):
         # run pygame
         pygame.init()
+        pygame.mixer.init()
 
         # create sprite groups
         self.menu = pygame.sprite.Group()
@@ -17,10 +19,14 @@ class Game:
 
         # initialize variables
         self.intro = True
+        self.clock = pygame.time.Clock()
+        self.fps = 30
         self.menu_screen = MenuScreen()
         self.game_screen = GameScreen()
-        self.santa = Santa()
         self.screen = self.menu_screen.get_screen()
+        self.santa = Santa(self.screen)
+        self.keys = [False, False, False, False]
+
 
         # draw buttons
         self.menu_screen.draw_buttons(self.start, self.quit_game)
@@ -39,6 +45,9 @@ class Game:
     # loop for entering menu
     def game_intro(self):
         while self.intro:
+            # Держим цикл на правильной скорости
+            self.clock.tick(self.fps)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.quit_game()
@@ -69,11 +78,35 @@ class Game:
 
         running = True
         while running:
+            # Держим цикл на правильной скорости
+            self.clock.tick(self.fps)
 
             # handling event
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.keys[0] = True
+                    elif event.key == pygame.K_DOWN:
+                        self.keys[1] = True
+                    elif event.key == pygame.K_RIGHT:
+                        self.keys[2] = True
+                    elif event.key == pygame.K_LEFT:
+                        self.keys[3] = True
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.keys[0] = False
+                    elif event.key == pygame.K_DOWN:
+                        self.keys[1] = False
+                    elif event.key == pygame.K_RIGHT:
+                        self.keys[2] = False
+                    elif event.key == pygame.K_LEFT:
+                        self.keys[3] = False
+
+            self.game_field.update(self.keys)
 
             # rendering
             self.screen.fill(BACKGROUND)
--- santa.py
+++ santa.py
@@ -2,15 +2,30 @@ import pygame
 
 
 class Santa(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, screen):
         super().__init__()
 
         # initialize image property
-        self.image = pygame.Surface((50, 50))
+        self.image = pygame.Surface((35, 50), pygame.SRCALPHA)
         self.image = pygame.image.load("images/santa.png")
+        self.image = pygame.transform.scale(self.image, (35, 50))
         # self.image.fill(color)
 
         # initialize rect property and sprite position
         self.rect = self.image.get_rect()
         self.rect.top = 0
         self.rect.left = 0
+
+        self.screen = screen
+
+    def update(self, keys):
+        if self.rect.left <= (self.screen.get_width() - self.image.get_width()) and keys[2]:
+            self.rect.x += 5
+        if keys[0]:
+            self.rect.y -= 5
+        elif keys[1]:
+            self.rect.y += 5
+        elif keys[3]:
+            self.rect.x -= 5
+
+
